51809109975a3c3ed1f2b4c5cbb46c32b0a1107f
programerkkkkkkkkkkkkkkkkkkk <hipitelo@outlook.com>
2023-03-10 10:43:03 +0900

    javascript regra de tres simples

--- www/components/login.js
+++ www/components/login.js
@@ -1,2 +1,16 @@
 // This is a JavaScript file
 
+function b(){
+var um,dois,tres,res
+
+um=document.getElementById('um').value
+dois=document.getElementById('dois').value
+tres=document.getElementById('tres').value
+
+
+ res=tres * dois/um;
+
+alert(res);
+
+}
+
